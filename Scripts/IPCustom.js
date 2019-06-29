b650925310b5e53e7c14c0674d63ac8c33734edb
Shawn <45581750+KOP-XIAO@users.noreply.github.com>
2019-06-29 18:27:32 +0800

    Update IPCustom.js

--- Scripts/IPCustom.js
+++ Scripts/IPCustom.js
@@ -5,16 +5,22 @@
 // var body = $response.body;
 // var obj = JSON.parse(body);
 
+if ($response.statusCode != 200) {
+  $done(Null);
+}
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
 const foods = ['🍎', '🍐', '🍊', '🍋', '🍌', '🍉', '🍇', '🍓', '🍈', '🍒', '🍑', '🥭', '🍅']
+const emojis= ['😄','😁','😢','🤗','😡','😴','🤑','👽','🤖','🎃']
 
-var title = foods[getRandomInt(foods.length)];
-var subtitle = '今天吃什么水果？';
-var ip = '1.1.1.1';
-var description = 'description'+ip;
-
+var body = $response.body;
+var obj = JSON.parse(body);
+var title = foods[getRandomInt(foods.length)]+' ' +obj['country'] + ' ' + emojis[getRandomInt(emojis.length)];
+var subtitle = obj['city'] + ' ' + obj['isp'];
+var ip = obj['query'];
+var description = obj['country'] + '\n' + obj['city'] + '\n' + obj['isp'] + '\n' + obj['ipType']+ '\n' + ip;
 
 $done({title, subtitle, ip, description});
