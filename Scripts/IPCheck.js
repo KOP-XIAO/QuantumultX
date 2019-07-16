7d1c8318f8940246e48df07673029057ed814f82
Shawn <45581750+KOP-XIAO@users.noreply.github.com>
2019-07-16 14:13:27 +0800

    Update IPCheck.js

--- Scripts/IPCheck.js
+++ Scripts/IPCheck.js
@@ -1,3 +1,10 @@
+// if ($response.statusCode != 200) {
+//   $done(Null);
+// }
+
+// var body = $response.body;
+// var obj = JSON.parse(body);
+
 if ($response.statusCode != 200) {
   $done(Null);
 }
