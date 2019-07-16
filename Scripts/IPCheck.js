f83da295eb68a930c83b7c9ccdd0715152dc9ca0
Shawn <45581750+KOP-XIAO@users.noreply.github.com>
2019-07-16 16:19:02 +0800

    Update IPCheck.js

--- Scripts/IPCheck.js
+++ Scripts/IPCheck.js
@@ -21,7 +21,7 @@ var flags = new Map([[ "AC" , "🇦🇨" ] , [ "AF" , "🇦🇫" ] , [ "AI" , "�
 var body = $response.body;
 var obj = JSON.parse(body);
 var title = flags.get(obj['countryCode']) + ' '+ obj['country'];
-var subtitle =''+ValidCheck(obj['city'])+'('+obj['org']+')'+'';
+var subtitle =''+ValidCheck(obj['city'])+'-'+'('+obj['org']+')'+'';
 var ip = obj['query'];
 var description = obj['country'] + '-' +ValidCheck(obj['city']) + '\n' + obj['org'] + '\n'+obj['ipName']+ '\n' + obj['query'];
 
