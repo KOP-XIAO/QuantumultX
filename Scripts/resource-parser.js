d4a151eb88a1a66e4feddd3d78cb0045c49a2ace
Shawn <45581750+KOP-XIAO@users.noreply.github.com>
2020-04-26 16:30:10 +0800

    更新使用说明

--- Scripts/resource-parser.js
+++ Scripts/resource-parser.js
@@ -1,20 +1,19 @@
 /** 
 
-#QuantumultX 资源解析器(2020-04-26: 15:30)
+#Quantumult X 资源解析器(2020-04-26: 16:20)
 
 本资源解析器作者: Shawn(@XIAO_KOP), 有问题请反馈:@Shawn_KOP_bot
 
-功能：将不同格式订阅转换成 QuantumultX，并提供下列可选参数
-   (目前支持 V2RayN/SSR/SS/Trojan/Quanx 订阅)
+功能: 将节点订阅解析成Quantumult X引用片段, 并提供下列可选参数(已支持 V2RayN/SSR/SS/Trojan/Quanx订阅)
 
-0️⃣ 请在订阅链接后加入"#"符号后加入下列参数，如 
+0️⃣ 在订阅链接后加入 "#" 符号后再加参数，不同参数间请使用 "&" 来连接，如:
        "#in=香港+台湾&emoji=1&tfo=1"
 
-1⃣️ 筛选参数 in、out, 分别为保留与排除，多个参数间用+号连接, 可直接使用中文(如"in=香港+台湾&out=BGP")
+1⃣️ 筛选参数 in、out, 分别为保留与排除，多个参数间用"+"号连接, 可直接使用中文(如"in=香港+台湾&out=BGP")
 
 2⃣️ emoji 参数为 emoji=1,2 或-1，为添加或删除节点名中的emoji旗帜（国行设备请用 emoji=2）
 
-3⃣️ udp=1，tfo=1 参数开启 udp-relay 及fast-open
+3⃣️ udp=1, tfo=1 参数开启udp-relay及fast-open.(默认关闭,且此参数对源类型为Quanx的链接无效)
 
 4⃣️ rename 重命名,rename=旧名@新名, 以及 "前缀@", "@后缀",用"+"连接，如 "rename=香港@HK+[SS]@+@[1X]
 
@@ -23,11 +22,11 @@
  */
 
 /**
- * 使用，
-0⃣️ 在quantumult X 配置文件中[general] 部分，加入 resource_parser_url=https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/master/Scripts/resource-parser.js
-1⃣️ 原始订阅连接为: https://raw.githubusercontent.com/crossutility/Quantumult-X/master/server-complete.txt , 
-2⃣️ 想要保留的参数为 in=tls+ss, 想要过滤的参数为 out=http+2, 请注意下面订阅链接后一定要加 ”#“ 符号
-3⃣️ 则填入 quanx 的总链接为  https://raw.githubusercontent.com/crossutility/Quantumult-X/master/server-complete.txt#in=tls+ss&out=http+2
+ * 使用说明，
+0⃣️ 在Quantumult X 配置文件中[general] 部分，加入 resource_parser_url=https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/master/Scripts/resource-parser.js
+1⃣️ 假设原始订阅连接为: https://raw.githubusercontent.com/crossutility/Quantumult-X/master/server-complete.txt , 
+2⃣️ 假设你想要保留的参数为 in=tls+ss, 想要过滤的参数为 out=http+2, 请注意下面订阅链接后一定要加 ”#“ 符号
+3⃣️ 则填入 Quanx 节点引用的的总链接为  https://raw.githubusercontent.com/crossutility/Quantumult-X/master/server-complete.txt#in=tls+ss&out=http+2
 4⃣️ 填入上述链接并打开的资源解析器开关
  */
 
