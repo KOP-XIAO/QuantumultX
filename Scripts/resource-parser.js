fcc311b24e0b54c441cf5c9c29870b48ed1f3b0c
Shawn <45581750+KOP-XIAO@users.noreply.github.com>
2021-08-29 21:20:12 +0800

    emoji

--- Scripts/resource-parser.js
+++ Scripts/resource-parser.js
@@ -1793,7 +1793,7 @@ function get_emoji(emojip, sname) {
     "🇫🇮": ["Finland", "芬兰","芬蘭","赫尔辛基"],
     "🇫🇷": ["FR", "France", "法国", "法國", "巴黎"],
     "🇬🇧": ["UK", "GB", "England", "United Kingdom", "英国", "伦敦", "英"],
-    "🇲🇴": ["MO", "Macao", "Mac", "澳门", "澳門", "CTM"],
+    "🇲🇴": ["MO", "Macao", "MAC", "澳门", "澳門", "CTM"],
     "🇰🇿": ["哈萨克斯坦"],
     "🇭🇺": ["匈牙利", "Hungary"],
     "🇱🇹": ["立陶宛"],
