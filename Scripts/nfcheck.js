9d7d101424e4b885a94e26d095bcbfdbb44c1509
Shawn <45581750+KOP-XIAO@users.noreply.github.com>
2021-09-28 12:02:22 +0800

    new

---
+++ Scripts/nfcheck.js
@@ -0,0 +1,128 @@
+/***
+
+Thanks to & modified from https://gist.githubusercontent.com/Hyseen/b06e911a41036ebc36acf04ddebe7b9a/raw/nf_check.js
+
+For Quantumult-X / [task-local]
+
+@XIAO_KOP
+
+**/
+
+
+const BASE_URL = 'https://www.netflix.com/title/'
+
+const FILM_ID = 81215567
+const link = { "media-url": "https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/master/img/southpark/7.png" } 
+const policy_name = "Netflix" //填入你的 netflix 策略组名
+
+const arrow = "➟"
+var output = ""
+
+var flags = new Map([[ "AC" , "🇦🇨" ] , [ "AF" , "🇦🇫" ] , [ "AI" , "🇦🇮" ] , [ "AL" , "🇦🇱" ] , [ "AM" , "🇦🇲" ] , [ "AQ" , "🇦🇶" ] , [ "AR" , "🇦🇷" ] , [ "AS" , "🇦🇸" ] , [ "AT" , "🇦🇹" ] , [ "AU" , "🇦🇺" ] , [ "AW" , "🇦🇼" ] , [ "AX" , "🇦🇽" ] , [ "AZ" , "🇦🇿" ] , [ "BB" , "🇧🇧" ] , [ "BD" , "🇧🇩" ] , [ "BE" , "🇧🇪" ] , [ "BF" , "🇧🇫" ] , [ "BG" , "🇧🇬" ] , [ "BH" , "🇧🇭" ] , [ "BI" , "🇧🇮" ] , [ "BJ" , "🇧🇯" ] , [ "BM" , "🇧🇲" ] , [ "BN" , "🇧🇳" ] , [ "BO" , "🇧🇴" ] , [ "BR" , "🇧🇷" ] , [ "BS" , "🇧🇸" ] , [ "BT" , "🇧🇹" ] , [ "BV" , "🇧🇻" ] , [ "BW" , "🇧🇼" ] , [ "BY" , "🇧🇾" ] , [ "BZ" , "🇧🇿" ] , [ "CA" , "🇨🇦" ] , [ "CF" , "🇨🇫" ] , [ "CH" , "🇨🇭" ] , [ "CK" , "🇨🇰" ] , [ "CL" , "🇨🇱" ] , [ "CM" , "🇨🇲" ] , [ "CN" , "🇨🇳" ] , [ "CO" , "🇨🇴" ] , [ "CP" , "🇨🇵" ] , [ "CR" , "🇨🇷" ] , [ "CU" , "🇨🇺" ] , [ "CV" , "🇨🇻" ] , [ "CW" , "🇨🇼" ] , [ "CX" , "🇨🇽" ] , [ "CY" , "🇨🇾" ] , [ "CZ" , "🇨🇿" ] , [ "DE" , "🇩🇪" ] , [ "DG" , "🇩🇬" ] , [ "DJ" , "🇩🇯" ] , [ "DK" , "🇩🇰" ] , [ "DM" , "🇩🇲" ] , [ "DO" , "🇩🇴" ] , [ "DZ" , "🇩🇿" ] , [ "EA" , "🇪🇦" ] , [ "EC" , "🇪🇨" ] , [ "EE" , "🇪🇪" ] , [ "EG" , "🇪🇬" ] , [ "EH" , "🇪🇭" ] , [ "ER" , "🇪🇷" ] , [ "ES" , "🇪🇸" ] , [ "ET" , "🇪🇹" ] , [ "EU" , "🇪🇺" ] , [ "FI" , "🇫🇮" ] , [ "FJ" , "🇫🇯" ] , [ "FK" , "🇫🇰" ] , [ "FM" , "🇫🇲" ] , [ "FO" , "🇫🇴" ] , [ "FR" , "🇫🇷" ] , [ "GA" , "🇬🇦" ] , [ "GB" , "🇬🇧" ] , [ "HK" , "🇭🇰" ] ,["HU","🇭🇺"], [ "ID" , "🇮🇩" ] , [ "IE" , "🇮🇪" ] , [ "IL" , "🇮🇱" ] , [ "IM" , "🇮🇲" ] , [ "IN" , "🇮🇳" ] , [ "IS" , "🇮🇸" ] , [ "IT" , "🇮🇹" ] , [ "JP" , "🇯🇵" ] , [ "KR" , "🇰🇷" ] , [ "LU" , "🇱🇺" ] , [ "MO" , "🇲🇴" ] , [ "MX" , "🇲🇽" ] , [ "MY" , "🇲🇾" ] , [ "NL" , "🇳🇱" ] , [ "PH" , "🇵🇭" ] , [ "RO" , "🇷🇴" ] , [ "RS" , "🇷🇸" ] , [ "RU" , "🇷🇺" ] , [ "RW" , "🇷🇼" ] , [ "SA" , "🇸🇦" ] , [ "SB" , "🇸🇧" ] , [ "SC" , "🇸🇨" ] , [ "SD" , "🇸🇩" ] , [ "SE" , "🇸🇪" ] , [ "SG" , "🇸🇬" ] , [ "TH" , "🇹🇭" ] , [ "TN" , "🇹🇳" ] , [ "TO" , "🇹🇴" ] , [ "TR" , "🇹🇷" ] , [ "TV" , "🇹🇻" ] , [ "TW" , "🇨🇳" ] , [ "UK" , "🇬🇧" ] , [ "UM" , "🇺🇲" ] , [ "US" , "🇺🇸" ] , [ "UY" , "🇺🇾" ] , [ "UZ" , "🇺🇿" ] , [ "VA" , "🇻🇦" ] , [ "VE" , "🇻🇪" ] , [ "VG" , "🇻🇬" ] , [ "VI" , "🇻🇮" ] , [ "VN" , "🇻🇳" ] , [ "ZA" , "🇿🇦"]])
+
+
+function ReadPolicy(cnt) {
+  var res = ""
+  for (var key in cnt) {
+    if (key == policy_name){
+      dd = cnt[key]
+      res = res + dd.join(arrow)
+      break
+    }
+  }
+  return res.split(policy_name+arrow)[1]
+}
+
+const message = {
+  action: "get_policy_state",
+  //content: dict
+
+};
+
+
+$configuration.sendMessage(message).then(resolve => {
+  if (resolve.error) {
+    console.log(resolve.error);
+  }
+  if (resolve.ret) {
+    output = ReadPolicy(resolve.ret)
+  }
+})
+
+
+
+!(async () => {
+  let result = {
+    title: 'Netflix 解锁检测',
+    subtitle: output,
+    content: '检测失败，请重试',
+  }
+  await Promise.race([test(FILM_ID),timeOut(5000)])
+  .then((code) => {
+    console.log(code)
+    
+    if (code === 'Not Available') {
+      result['content'] = '该节点未解锁 Netflix'
+      //return 
+      //console.log(result)
+    } else if (code === 'Not Found') {
+      result['content'] = '该节点仅支持解锁 Netflix 自制剧'
+      //return
+    } else if (code === "timeout") {
+      result['content'] = "测试超时"
+    } else {
+      result['content'] = '该节点完整解锁 Netflix ➟ ⟦'+flags.get(code.toUpperCase())+" 地区⟧"
+    }
+    
+    $notify(result["title"], output, result["content"], link)
+    
+    //console.log(result)
+    $done()
+  })
+})()
+.finally(() => $.done());
+
+function timeOut(delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      //reject(new Error('timeout'))
+      resolve("timeout")
+    }, delay)
+  })
+}
+
+function test(filmId) {
+  return new Promise((resolve, reject) => {
+    let option = {
+      url: BASE_URL + filmId,
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
+      },
+    }
+    $task.fetch(option).then (response => {
+      console.log(response.statusCode)
+      if (response.statusCode === 404) {
+        resolve('Not Found')
+        return
+      }
+
+      if (response.statusCode === 403) {
+        resolve('Not Available')
+        return
+      }
+
+      if (response.statusCode === 200) {
+        let url = response.headers['X-Originating-URL']
+        let region = url.split('/')[3]
+        region = region.split('-')[0]
+        if (region == 'title') {
+          region = 'us'
+        }
+        resolve(region)
+        return
+      }
+      reject('Error')
+    })
+  })
+}
