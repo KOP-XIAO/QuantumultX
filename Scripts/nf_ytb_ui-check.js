2986ee687e3f69148f14f5ce0b4451f178f44e42
Shawn <45581750+KOP-XIAO@users.noreply.github.com>
2021-09-29 20:34:43 +0800

    Update nf_ytb_ui-check.js

--- Scripts/nf_ytb_ui-check.js
+++ Scripts/nf_ytb_ui-check.js
@@ -129,6 +129,7 @@ function testYTB() {
   return new Promise((resolve, reject) => {
     let option = {
       url: BASE_URL_YTB,
+      opts: opts,
       headers: {
         'User-Agent':
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36'
