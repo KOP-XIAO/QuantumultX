038006f6475471285742f48eb53adebc26f6c68d
dsmn <wong09998@gmail.com>
2022-12-15 18:05:18 +0800

    Update nfcheck.js

--- Scripts/nfcheck.js
+++ Scripts/nfcheck.js
@@ -11,7 +11,7 @@ For Quantumult-X / [task-local]
 
 const BASE_URL = 'https://www.netflix.com/title/'
 
-const FILM_ID = 81215567
+const FILM_ID = 81280792
 const link = { "media-url": "https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/master/img/southpark/7.png" } 
 const policy_name = "Netflix" //填入你的 netflix 策略组名
 
